69fdc9b7544d8b039e1e446781128d743c0404a2
JessicaQueiroz <jehqueiroz01@gmail.com>
2020-10-02 10:51:54 -0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+    	// estrutura de dados que armazena uma coleção de elementos de tal forma que
+		// cada um dos elementos possa ser identificado,por, pelo menos, um índice ou uma chave.
+		// tipo[]nome = {....} nome[i] i - índice/posicao i[0] i[1] i[2]...
+	    String[] nomes = {"Jéssica", "Ana", "Miguel"};
+	    int[] numeros = {20,50,163,259};
+	    boolean[] itens = {true,false,true,false};
+
+	    System.out.println(nomes[0]);
+	    System.out.println(numeros[2]);
+	    System.out.println(itens[3]);
+    }
+}
